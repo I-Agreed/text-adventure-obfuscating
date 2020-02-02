688cedbacb10e21d264d435384c91f0209cd1906
Brendan <48776053+I-Agreed@users.noreply.github.com>
2020-02-02 23:46:25 +1100

    removed some brackets

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ a=1
 l="You are standing at the entrance to a long"+t
 x="look"
 n=", "+x+"\n> "
-h=['',f", {b}"]
-q=[[f,x],[f,b,x]]
-w=[["p(d);r=1","p(l)"],["r=2;p('You have escaped the'+t,'You win!')","p(l);r=0","print(d)"]]
+h='',f", {b}"
+q=[f,x],[f,b,x]
+w=["p(d);r=1","p(l)"],["r=2;p('You have escaped the'+t,'You win!')","p(l);r=0","print(d)"]
 while r != 2:exec(w[r][q[r].index(input(c+f+h[r]+n))])
