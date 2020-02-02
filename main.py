aec09871aaaa332614439c8cc4d65ef351319bea
P4GAN <55480921+P4GAN@users.noreply.github.com>
2020-02-02 23:44:27 +1100

    fixed

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ p=print
 a=1
 l="You are standing at the entrance to a long"+t
 x="look"
-n=", ",x,"\n> "
-h=[c+f,c+f+b]
+n=", "+x+"\n> "
+h=['',f", {b}"]
 q=[[f,x],[f,b,x]]
 w=[["p(d);r=1","p(l)"],["r=2;p('You have escaped the'+t,'You win!')","p(l);r=0","print(d)"]]
-while r != 2:exec(w[r][q[r].index(input(h[r]))])
+while r != 2:exec(w[r][q[r].index(input(c+f+h[r]+n))])
