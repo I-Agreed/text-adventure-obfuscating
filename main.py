3b19b910f48062e63998c7ed9ae3d1782fad4e24
P4GAN <55480921+P4GAN@users.noreply.github.com>
2020-02-02 22:34:16 +1100

    ok finally put the version in

--- main.py
+++ main.py
@@ -2,17 +2,21 @@ r=0
 f="forward"
 b="backward"
 c="actions available: "
-d="You are in the depths of the tunnel."
+t=" tunnel."
+d="You are in the depths of the"+t
 p=print
 a=1
-l="You are standing at the entrance to a long tunnel."
+l="You are standing at the entrance to a long"+t
+x="look"
+n=", ",x,"\n> "
+z=input
 while a!=f:
   if r<1:
-    a=input(c+f+", look\n> ")
-    if a=="look":p(l)
+    a=z(c+f+n)
+    if a==x:p(l)
     elif a==f:p(d);r=1
-  if r==1:
-    a=input(c+f+", {b}, look\n> ")
+  if r>0:
+    a=z(c+f+f", {b}"+n)
     if a==b:p(l);r=0
-    elif a=="look":p(d)
-p("You have escaped the tunnel. You win!")
+    elif a==x:p(d)
+p("You have escaped the"+t,"You win!")
