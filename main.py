e6aa60ed6df0c864eeec1ea72b18f18bd21d0728
I_Agreed <48776053+I-Agreed@users.noreply.github.com>
2020-09-11 20:59:50 +1000

    used f string to save a byte

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ d="You are in the depths of the"+t
 p=print
 l="You are standing at the entrance to a long"+t
 x="look"
-n=", "+x+"\n> "
+n=f", {x}\n> "
 h='',", "+b
 q=f,x,b
 w=["p(d);r=1","p(l)"],["r=2;p('You have escaped the'+t,'You win!')","p(d)","p(l);r=0"]
